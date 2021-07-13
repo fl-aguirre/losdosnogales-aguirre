92a7603e47bbf9d856df72ef2ac17d9851c49296
Franco Aguirre <frl.aguirre@gmail.com>
2021-07-13 13:38:01 -0300

    Contador con props

--- src/componentes/ItemCount.jsx
+++ src/componentes/ItemCount.jsx
@@ -1,34 +1,7 @@
-import { useState, useEffect } from 'react';
 import Card from 'react-bootstrap/Card';
 import Button from 'react-bootstrap/Button';
 
-const ItemCount = () => {
-
-    const [stock, setStock] = useState(10)
-    const [cantidad, setCantidad] = useState(0)
-
-    const onAdd = () => {
-        setCantidad(cantidad + 1)
-        if (cantidad == 10) {
-            setCantidad(stock)
-            alert("No hay más stock!")
-        }
-    }
-
-    const onSub = () => {
-        setCantidad(cantidad - 1)
-        if (cantidad == 0) {
-            setCantidad(0)
-        }
-    }
-
-    const addCart = () => {
-        if (cantidad === 0){
-            alert('No has agregado productos!')
-        }else {
-            alert('Has agregado ' + cantidad + ' productos!')
-        }
-    }
+const ItemCount = ({cantidad, onAdd, onSub, addCart}) => {
 
     return (
         <Card className="text-center">
@@ -40,7 +13,7 @@ const ItemCount = () => {
                 <Button size="sm" onClick={onAdd}>+</Button>
                 <Button size="sm" onClick={onSub}>-</Button>
 
-                <span>Cantidad: {cantidad}</span><br/>
+                <span> Cantidad: {cantidad}</span><br/>
 
                 <Button size="sm" onClick={addCart}>Agregar al carrito</Button>
             </Card.Body>
--- src/componentes/ItemListContainer.jsx
+++ src/componentes/ItemListContainer.jsx
@@ -1,3 +1,4 @@
+import { useState, useEffect } from 'react';
 import Items from "./Items";
 import CardGroup from 'react-bootstrap/CardGroup';
 import ItemCount from "./ItemCount";
@@ -5,12 +6,43 @@ import ItemCount from "./ItemCount";
 function ItemListContainer() {
     const arrayProductos = ['Producto 1', 'Producto 2', 'Producto 3']
 
+    const [stock, setStock] = useState(10)
+    const [cantidad, setCantidad] = useState(0)
+
+    const onAdd = () => {
+        setCantidad(cantidad + 1)
+        if (cantidad == 10) {
+            setCantidad(stock)
+            alert("No hay más stock!")
+        }
+    }
+
+    const onSub = () => {
+        setCantidad(cantidad - 1)
+        if (cantidad == 0) {
+            setCantidad(0)
+        }
+    }
+
+    const addCart = () => {
+        if (cantidad === 0){
+            alert('No has agregado productos!')
+        }else {
+            alert('Has agregado ' + cantidad + ' productos!')
+        }
+    }
+
     return (
         <CardGroup>
-            <ItemCount />
+            <ItemCount 
+                cantidad = {cantidad}
+                onAdd = {onAdd}
+                onSub = {onSub}
+                addCart = {addCart}
+            />
             <Items 
-            productos={arrayProductos}
-            greeting="La mejor garantía"
+                productos={arrayProductos}
+                greeting="La mejor garantía"
             />
         </CardGroup>
     )
